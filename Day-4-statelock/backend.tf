60568b95d35036c4d3d08ae137ec057b49189e63
Ganji Sai_Kiran <ganjisaikiran04@gmail.com>
2026-07-14 14:33:16 +0530

    Day-4

--- Day-4-statelock/backend.tf
+++ Day-4-statelock/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "bucketforfractice4477"
-    key    = "bucketforfractice4477"
-    region = "ap-southeast-2"
-    use_locking = true
+    bucket         = "bucketforfractice4477"
+    key            = "bucketforfractice4477"
+    region         = "ap-southeast-2"
+    dynamodb_table = "terraform-locks"
   }
 }
--- Day-4-statelock/main.tf
+++ Day-4-statelock/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
   ami = "ami-0aa77def67762002c"
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
   tags = {
     name = "EC2-1"
   }
