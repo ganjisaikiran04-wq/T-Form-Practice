32c6977f6d23ae35ebfe1208840660652f1adfc9
Ganji Sai_Kiran <ganjisaikiran04@gmail.com>
2026-07-17 17:03:23 +0530

    day-9

---
+++ Day-9-passing-values-to-module/main.tf
@@ -0,0 +1,14 @@
+module "test" {
+    source = "../Day-9-module-source"
+    cidr_block = "10.0.0.0/16"
+    subnet1_cidr_block = "10.0.1.0/24"
+    subnet2_cidr_block = "10.0.2.0/24"
+    igw_name = "IGW"
+    route_table_name = "RouteTable"
+    sg_name = "SecurityGroup"
+    ec2_instance_ami = "ami-0aa77def67762002c"
+    ec2_instance_type = "t3.micro"
+    ec2_instance_name = "EC2Instance"
+
+}
+ 
